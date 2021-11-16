28d82b1fd152423f51f55bdc447b263148025e60
TheRusmith <mpiscitelli98@gmail.com>
2021-11-16 12:33:16 +0100

    jn

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, worljgfkuknbhbkjhyfd!")
+            Text("Hello, worljgfkuknbhbhbhhbkjhyfd!")
                 .padding()
             
             Text("Test 1")
