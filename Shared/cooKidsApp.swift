0114b99f722fd465d05958770783ab91e20f7e6f
Gregor Hermanowski <gregor@10-20-50-198.0200.fed.it.iosda.org>
2021-11-16 11:55:29 +0100

    Project setup

---
+++ Shared/cooKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cooKidsApp.swift
+//  Shared
+//
+//  Created by Gregor Hermanowski on 16.November.
+//
+
+import SwiftUI
+
+@main
+struct cooKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
