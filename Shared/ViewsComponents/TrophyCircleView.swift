44350b58232ddf48209be36c89013448f6f5a596
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2021-11-18 11:20:24 +0100

    Refactored TrophyCircleView to use model data

--- Shared/ViewsComponents/TrophyCircleView.swift
+++ Shared/ViewsComponents/TrophyCircleView.swift
@@ -9,49 +9,45 @@ import Foundation
 import SwiftUI
 
 struct TrophyCircleView: View {
-    let hasCaption : Bool
-    let text : String
-    let numberOnNumber : String?
-    let emojiIcon : String
-    let progressFrom0to1 : Double
-    
-    
-    
-    var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .fill(LinearGradient(colors: [.orange.opacity(0.1), .orange.opacity(0.6)], startPoint: .top, endPoint: .bottom))
-                    .overlay(ProgressCircleStroke(progress: progressFrom0to1))
-                .padding(.top, 5)
-                .padding(.horizontal, 5)
-                .frame(width: 100, height: 100)
-                Text(emojiIcon)
-                    .font(.system(size: 62))
-            }
-            Text(text)
-                .font(.title3)
-            if hasCaption {
-            Text(numberOnNumber ?? "")
-                .font(.callout)
-                .foregroundColor(.secondary)}
-        }
-    }
-}
-
-struct ProgressCircleStroke : View {
-    let progress : Double
-    var body: some View {
-    Circle()
-        .trim(from: 0, to: progress)
-        .stroke(Color.orange, style: .init(lineWidth: 8, lineCap: .round))
-        .rotationEffect(Angle(degrees: -90.0))
-    }
+	@EnvironmentObject private var userStore: UserStore
+	
+	let trophy: Trophy
+	
+	func progressCircle(progress: Double) -> some View {
+		Circle()
+			.trim(from: 0, to: progress)
+			.stroke(Color.orange, style: .init(lineWidth: 8, lineCap: .round))
+			.rotationEffect(Angle(degrees: -90.0))
+	}
+	
+	var body: some View {
+		VStack {
+			if let trophyDetails = userStore.trophies[trophy] {
+				ZStack {
+					Circle()
+						.fill(.linearGradient(colors: [.orange.opacity(0.1), .orange.opacity(0.6)],
+											  startPoint: .top,
+											  endPoint: .bottom))
+						.overlay(progressCircle(progress: trophyDetails.progress))
+						.padding(.top, 5)
+						.padding(.horizontal, 5)
+						.frame(width: 100, height: 100)
+					
+					Text(trophy.icon)
+						.font(.system(size: 62))
+				}
+				
+				Text("\(trophyDetails.count) / \(trophyDetails.maximum)")
+					.font(.title3)
+			}
+		}
+	}
 }
 
 
 struct TrophyCircleView_Previews: PreviewProvider {
-    static var previews: some View {
-        TrophyCircleView(hasCaption: true, text: "Sweets", numberOnNumber: "6/10", emojiIcon: "🍫", progressFrom0to1: 0.6)
-    }
+	static var previews: some View {
+		TrophyCircleView(trophy: .baking)
+			.environmentObject(UserStore())
+	}
 }
