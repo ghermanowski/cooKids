8418d43fd7bdd6cc9561c51e1b1746d1f1403839
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2021-11-18 15:31:26 +0100

    Small code style improvements

--- Shared/Recipes.swift
+++ Shared/Recipes.swift
@@ -8,37 +8,36 @@
 import SwiftUI
 
 struct Recipes: View {
-    @EnvironmentObject private var userStore: UserStore
-    
-
-    var body: some View{
-        NavigationView{
-            ScrollView(.vertical) {
-                VStack(alignment: .leading, spacing: .zero) {
-                   
-                    ForEach(Recipe.Category.allCases) { category in
-                        Text(category.rawValue).font(.largeTitle).padding([.top, .horizontal])
-                        
-                        ScrollView(.horizontal, showsIndicators: false){
-                            HStack(spacing: 20) {
-                                ForEach(userStore.recipes.filter { $0.category == category}) { recipe in
-                                    RecipeView(recipe: recipe)
-                                }
-                            }.padding(.horizontal)
-                        }
-                    }
-                }
-            }.navigationTitle("Recipes")
-        }
-    }
-    
+	@EnvironmentObject private var userStore: UserStore
+	
+	var body: some View{
+		NavigationView {
+			ScrollView(.vertical) {
+				VStack(alignment: .leading, spacing: .zero) {
+					ForEach(Recipe.Category.allCases) { category in
+						Text(category.rawValue)
+							.font(.largeTitle)
+							.padding([.top, .horizontal])
+						
+						ScrollView(.horizontal, showsIndicators: false) {
+							HStack(spacing: 20) {
+								ForEach(userStore.recipes.filter { $0.category == category }) { recipe in
+									RecipeView(recipe: recipe)
+								}
+							}
+							.padding(.horizontal)
+						}
+					}
+				}
+			}
+			.navigationTitle("Recipes")
+		}
+	}
 }
 
-
-
-
 struct Recipes_Previews: PreviewProvider {
-    static var previews: some View {
-        Recipes()            .environmentObject(UserStore())
-    }
+	static var previews: some View {
+		Recipes()
+			.environmentObject(UserStore())
+	}
 }
