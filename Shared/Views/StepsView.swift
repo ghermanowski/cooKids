4407ba131f3759a0b06d05dd49abf6be6e9ed540
enni7 <annaizzo7@hotmail.it>
2021-11-22 22:38:05 +0100

    Steps view, not definitive

--- Shared/Views/StepsView.swift
+++ Shared/Views/StepsView.swift
@@ -2,37 +2,75 @@
 //  StepsView.swift
 //  cooKids
 //
-//  Created by Anna Izzo on 22/11/21.
+//  Created on 22/11/21.
 //
 
 import SwiftUI
 
 struct StepsView: View {
-	var thisRecipe: Recipe
+	let thisRecipe: Recipe
+	@State var stepCount = 0
+	@State var showTakePicture = false
+	@State private var showAlert = false
 	
 	var body: some View {
 		VStack(spacing: 30){
 			
-			StepsImage(image: thisRecipe.imageName, introOrNot: false)
+			StepsImage(image: thisRecipe.steps[stepCount].imageName ?? "", introOrNot: false)
 				.padding(.top, 30)
 			
-			Text("")
+			Text(thisRecipe.steps[stepCount].title)
 				.font(.system(.title, design: .rounded))
 				.padding(.horizontal, 20)
-			Text("")
+			Text(thisRecipe.steps[stepCount].body)
 				.font(.system(.title2, design: .rounded))
-
+				.padding(.horizontal, 20)
 			
 			Spacer()
 			
-//			NavigationLink(destination: StepsView(thisRecipe: thisRecipe), isActive: $showNext) {EmptyView() }
-//			BigButtonBottom(buttonText: "Next", systemIcon: "") { }
+			NavigationLink(destination: TakeAPictureView(thisRecipe: thisRecipe), isActive: $showTakePicture) {
+				RoundedRectangle(cornerRadius: 10)
+					.fill(.orange)
+					   .frame(maxWidth: .infinity, maxHeight: 60)
+					   .overlay {
+						   Text("Next")
+							   .foregroundColor(.white)
+							   .font(.system(size: 24, weight: .bold, design: .rounded))
+					   }
+			}
+			.onTapGesture {
+				if stepCount < (thisRecipe.steps.count - 1) {
+					stepCount += 1
+				} else {
+					showTakePicture.toggle()
+				}
+			}
+			.padding(20)
+			.background(.ultraThinMaterial)
+			.shadow(color: .clear, radius: .zero)
+			.shadow(radius: 4, y: -2)
+			
+		}
+		.navigationBarTitleDisplayMode(.inline)
+		.navigationTitle("Step \(stepCount + 1)")
+		.navigationBarItems(trailing: Button(action: {
+			showAlert.toggle()
+		}, label: {
+			Image(systemName: "xmark.circle.fill")
+				.foregroundColor(.gray)
+		}))
+		.alert("Are you sure you want to stop?", isPresented: $showAlert) {
+			Button("Stop", role: .destructive) {
+				//back to recipes
+			}
+		} message: {
+			Text("You will lose your progress")
 		}
 	}
 }
 
 struct StepsView_Previews: PreviewProvider {
-    static var previews: some View {
-		StepsView(thisRecipe: (UserStore().recipes[7]))
-    }
+	static var previews: some View {
+		StepsView(thisRecipe: (UserStore().recipes[0]))
+	}
 }
