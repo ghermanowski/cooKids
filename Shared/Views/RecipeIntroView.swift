0de347dc9dc6b14c0c436419b4c78e044467517d
enni7 <annaizzo7@hotmail.it>
2021-11-22 22:35:37 +0100

    A temporary navigationView to intro, for trying the next button

--- Shared/Views/RecipeIntroView.swift
+++ Shared/Views/RecipeIntroView.swift
@@ -2,39 +2,40 @@
 //  RecipeIntroView.swift
 //  cooKids
 //
-//  Created by Anna Izzo on 18/11/21.
+//  Created on 18/11/21.
 //
 
 import SwiftUI
 
 struct RecipeIntroView: View {
 	@EnvironmentObject private var userStore: UserStore
-	var thisRecipe: Recipe
+	let thisRecipe: Recipe
 	
 	var body: some View {
-		VStack {
-			ScrollView (.vertical) {
-				VStack (spacing: 30) {
-					StepsImage(image: thisRecipe.imageName, introOrNot: true)
-						.padding(.top, 30)
-					
-					ScrollView (.horizontal) {
-						HStack (spacing: 20) {
-							ForEach(thisRecipe.trophies) { trophy in                                TrophyCircleView(trophy: trophy, showProgressText: false)
-									.scaleEffect(0.86)
+		NavigationView {
+			VStack {
+				ScrollView (.vertical) {
+					VStack (spacing: 30) {
+						StepsImage(image: thisRecipe.imageName, introOrNot: true)
+							.padding(.top, 30)
+						
+						ScrollView (.horizontal) {
+							HStack (spacing: 20) {
+								ForEach(thisRecipe.trophies) { trophy in                                TrophyCircleView(trophy: trophy, showProgressText: false)
+										.scaleEffect(0.86)
+								}
 							}
-						}
-					}.padding(.horizontal, 20)
-					
-					Text("Ingredients")
-						.font(.system(size: 30, weight: .semibold, design: .rounded))
-					IngredientsListView(ingredients: thisRecipe.ingredients, withChecking: false)
-						.padding(.horizontal, 20)
+						}.padding(.horizontal, 20)
+						
+						Text("Ingredients")
+							.font(.system(size: 30, weight: .semibold, design: .rounded))
+						IngredientsListView(ingredients: thisRecipe.ingredients, withChecking: false)
+							.padding(.horizontal, 20)
+					}
 				}
-			}
-			Spacer()
-			NavigationLink(destination: IngredientStepView(thisRecipe: thisRecipe)) {
-				RoundedRectangle(cornerRadius: 10)
+				Spacer()
+				NavigationLink(destination: IngredientStepView(thisRecipe: thisRecipe)) {
+					RoundedRectangle(cornerRadius: 10)
 						.fill(.orange)
 						.frame(maxWidth: .infinity, maxHeight: 60)
 						.padding(20)
@@ -46,15 +47,17 @@ struct RecipeIntroView: View {
 								.foregroundColor(.white)
 								.font(.system(size: 24, weight: .bold, design: .rounded))
 						}
+				}
 			}
 		}
-		.navigationBarTitleDisplayMode(.inline)
+		.navigationBarTitleDisplayMode(.large)
+		.navigationTitle(thisRecipe.title)
 	}
 }
 
 struct RecipeIntroView_Previews: PreviewProvider {
 	static var previews: some View {
-		RecipeIntroView(thisRecipe: (UserStore().recipes[7]))
+		RecipeIntroView(thisRecipe: (UserStore().recipes[0]))
 			.environmentObject(UserStore())
 	}
 }
