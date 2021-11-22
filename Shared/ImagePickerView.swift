296e8159cf57d3a30db6900d13e2dee5f4714d5b
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2021-11-22 09:57:42 +0100

    Moved Coordinator into ImagePickerView

--- Shared/ImagePickerView.swift
+++ Shared/ImagePickerView.swift
@@ -9,10 +9,9 @@ import UIKit
 import SwiftUI
 
 struct ImagePickerView : UIViewControllerRepresentable {
+	@Environment(\.dismiss) var dismiss
 	
 	@Binding var selectedImage: UIImage?
-	
-	@Environment(\.presentationMode) var isPresented
 	
 	func makeUIViewController(context: Context) -> UIImagePickerController {
 		let imagePicker = UIImagePickerController()
@@ -25,8 +24,23 @@ struct ImagePickerView : UIViewControllerRepresentable {
 		return Coordinator(picker: self)
 	}
 	
-	func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
-		
-	}
+	func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) { }
 	
+	class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+		private let picker: ImagePickerView
+		
+		init (picker: ImagePickerView) {
+			self.picker = picker
+		}
+		
+		/*
+		 This method is coming from UIImagePickerControllerDelegate. It will be triggered when the user captured a pic using Camera or when the user selects an image from the photo library (not our case). We are getting the image from info[.originalImage] and converting it into a UIImage object. Then assigning that to “selectedImage” variable of ImagePickerView (picker) object.
+		 */
+		func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+			guard let selectedImage = info[.originalImage] as? UIImage else { return }
+			self.picker.selectedImage = selectedImage
+			// Dismisses the view after picking the image
+			self.picker.dismiss()
+		}
+	}
 }
