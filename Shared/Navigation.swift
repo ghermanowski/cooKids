7a23bf9c5c77bea56b37f01979b97f38d37c0439
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2021-11-22 09:22:38 +0100

    Added workaround to use rounded navigation bar font

--- Shared/Navigation.swift
+++ Shared/Navigation.swift
@@ -8,6 +8,28 @@
 import SwiftUI
 
 struct Navigation: View {
+	internal init() {
+		// WORKAROUND: Sets the navigation title font design to rounded
+		let navigationBarAppearance = UINavigationBarAppearance()
+		var titleFont = UIFont.boldSystemFont(ofSize: UIFont.buttonFontSize)
+		
+		if let descriptor = titleFont.fontDescriptor.withDesign(.rounded) {
+			titleFont = UIFont(descriptor: descriptor, size: UIFont.buttonFontSize)
+		}
+		
+		navigationBarAppearance.titleTextAttributes = [.font: titleFont]
+		
+		var largeTitleFont = UIFont.boldSystemFont(ofSize: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize)
+		
+		if let descriptor = largeTitleFont.fontDescriptor.withDesign(.rounded) {
+			largeTitleFont = UIFont(descriptor: descriptor, size: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize)
+		}
+		
+		navigationBarAppearance.largeTitleTextAttributes = [.font: largeTitleFont]
+		
+		UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+	}
+	
 	@SceneStorage("activeTab") private var activeTab = Tab.recipes
 	
 	enum Tab: Int {
@@ -27,6 +49,7 @@ struct Navigation: View {
 
 struct Navigation_Previews: PreviewProvider {
     static var previews: some View {
-		Navigation().environmentObject(UserStore())
+		Navigation()
+			.environmentObject(UserStore())
     }
 }
