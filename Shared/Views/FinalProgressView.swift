f4391eda829f37cafeb7b7d643b5105b4f10499f
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2021-11-23 21:52:31 +0100

    Finalised FinalProgressView, added unlocked recipes

--- Shared/Views/FinalProgressView.swift
+++ Shared/Views/FinalProgressView.swift
@@ -12,47 +12,77 @@ struct FinalProgressView: View {
 	
 	@Binding var isPresented: Bool
 	
+	@State var previousTotalTrophyProgress: Int = .max
+	
 	let recipe: Recipe
 	
 	var body: some View {
-		ZStack {
-			VStack(alignment: .leading, spacing: 30) {
-				Group {
-					Text("You have made progress in:")
-						.font(.system(.title2, design: .rounded))
-					
-					ScrollView(.horizontal) {
-						HStack(spacing: 30) {
-							ForEach(recipe.trophies) { trophy in
-								TrophyCircleView(trophy: trophy, showProgressText: false, showProgressBar: true)
-									.padding(5)
+		ScrollView {
+			VStack(alignment: .leading, spacing: 20) {
+				Text("You have made progress in:")
+					.font(.system(.title, design: .rounded).weight(.medium))
+					.padding(.leading, 20)
+					.onAppear {
+						previousTotalTrophyProgress = userStore.totalTrophyProgress
+						
+						Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+							for trophy in recipe.trophies {
+								withAnimation {
+									userStore.trophies[trophy]!.count += 1
+								}
 							}
 						}
 					}
+				
+				LazyVGrid(columns: Array(repeating: .init(.flexible(minimum: .zero, maximum: .infinity),
+														  spacing: 20,
+														  alignment: .top),
+										 count: 3),
+						  spacing: 30) {
+					let trophies = recipe.trophies.sorted {
+						userStore.trophies[$0]!.progress > userStore.trophies[$1]!.progress
+					}
 					
-					Text("New recipes unlocked:")
-						.font(.system(.title2, design: .rounded))
-					
-					ScrollView(.horizontal) {
-						HStack(spacing: 30) {
-							//
+					ForEach(trophies) { trophy in
+						TrophyCircleView(trophy: trophy, showProgressText: false, showProgressBar: true)
+					}
+				}
+				.padding(.horizontal)
+				.padding(.bottom, 10)
+				
+				let unlockedRecipes = userStore.recipes.filter {
+					$0.requiredTrophyProgress > previousTotalTrophyProgress &&
+						$0.requiredTrophyProgress < userStore.totalTrophyProgress
+				}
+				
+				if !unlockedRecipes.isEmpty {
+					VStack(alignment: .leading, spacing: 10) {
+						Text("New recipes unlocked:")
+							.font(.system(.title, design: .rounded).weight(.medium))
+							.padding(.leading, 20)
+						
+						ScrollView(.horizontal, showsIndicators: false) {
+							HStack(spacing: 30) {
+								ForEach(unlockedRecipes) { recipe in
+									RecipeCard(recipe: recipe)
+								}
+							}
+							.padding(.horizontal, 30)
+							.padding(.vertical, 10)
 						}
 					}
 				}
-				.padding(.horizontal, 20)
-				
-				Spacer()
-				
-				BigButtonBottom(buttonText: "Done", systemIcon: "checkmark") {
-					isPresented = false
-				}
 			}
-//			.padding(.horizontal, 20)
 			.padding(.top, 30)
 		}
-		.navigationTitle("Progress")
+		.navigationTitle("Well done!")
 		.navigationBarTitleDisplayMode(.large)
 		.navigationBarBackButtonHidden(true)
+		.safeAreaInset(edge: .bottom) {
+			BigButtonBottom(buttonText: "Done", systemIcon: "checkmark") {
+				isPresented = false
+			}
+		}
 	}
 }
 
